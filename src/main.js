04be961999973727dd90754ec1b7c1c2efd08dcb
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 09:45:46 -0500

    feat(state-management): integrate pinia for state management.

--- src/main.js
+++ src/main.js
@@ -18,8 +18,10 @@ import {
 } from "primevue";
 import i18n from "./i18n.js";
 import router from "./router.js";
+import pinia from "./pinia.js";
 
 createApp(App)
+    .use(pinia)
     .use(router)
     .use(i18n)
     .use(PrimeVue, { theme: { preset: Material}, ripple: true})
